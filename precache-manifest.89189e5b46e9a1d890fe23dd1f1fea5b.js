69b83a08d90443cac6ca0220f394673e3bdd067c
Ben Wexler <mixedywex@gmail.com>
2020-04-26 16:39:49 -0400

    Updates

---
+++ precache-manifest.89189e5b46e9a1d890fe23dd1f1fea5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7aac8ed8429370c90ec1a2b9c76128",
+    "url": "/googleStockClone/index.html"
+  },
+  {
+    "revision": "fc0d7edcff6117c758f1",
+    "url": "/googleStockClone/static/css/main.7a267aa8.chunk.css"
+  },
+  {
+    "revision": "b6a62641d22b644663f9",
+    "url": "/googleStockClone/static/js/2.44058c6a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/googleStockClone/static/js/2.44058c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0d7edcff6117c758f1",
+    "url": "/googleStockClone/static/js/main.f651c7a7.chunk.js"
+  },
+  {
+    "revision": "bbc056c51fc8c25d6602",
+    "url": "/googleStockClone/static/js/runtime-main.eeb24582.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/googleStockClone/precache-manifest.89189e5b46e9a1d890fe23dd1f1fea5b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/googleStockClone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
